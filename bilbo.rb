fad071b52ee674e86b2d0c5bd42c18a74dd7f763
dan5 <dan5.ya@gmail.com>
2009-10-29 12:03:02 +0900

    * WEBrickサーバーとして起動できるようにした * viewファイルを追加

--- bilbo.rb
+++ bilbo.rb
@@ -1,12 +1,34 @@
 # -*- encoding: UTF-8 -*-
 
-# helper.rb
+# helper.rb ----------------
 require 'pathname'
-BILBO_VERSION = '0.1.0'
+BILBO_VERSION = '0.1.1'
 def chdir(key) Dir.chdir(config[:dir][key]) { yield }; end
 def load_plugins() Dir.glob("#{config[:dir][:plugins]}/*.rb").sort.each {|e| load e }; end
+#include Rack::Utils; alias_method :h, :escape_html
 
-# model.rb
+#def root_path
+#  ENV['SCRIPT_NAME'] ? File.dirname(ENV['SCRIPT_NAME']) : ''
+#end
+
+def link_to(name, options = {})
+  controller = options.delete(:controller)
+  label = '#' + label if label = options.delete(:label)
+  params = options.keys.map {|e| "#{e}=#{options[e]}"}.join('&').sub(/action=/, '')
+  %Q!<a href="#{root_path}/#{controller}#{options.empty? ? '' : '?'}#{params}#{label}">#{name}</a>!
+end
+
+def add_plugin_hook(key, priority = 128, &block) # todo: priority
+  $hook_procs ||= {}
+  $hook_procs[key] ||= []
+  $hook_procs[key] << block
+end
+
+def render_plugin_hook(key)
+  (($hook_procs or {})[key] or []).map(&:call).join("\n")
+end
+
+# model.rb ----------------
 class Entry
   attr_reader :filename
   def initialize(filename)
@@ -17,25 +39,33 @@ class Entry
     filename.basename('.*')
   end
 
+  def read_data
+    @data_read ||= chdir(:entries) { filename.read }.split(/^__$/, 2)
+  end
+
+  def header
+    read_data[1] ? read_data[0] : nil
+  end
+
   def body
-    @body ||= chdir(:entries) { filename.read }
+    read_data[1] or read_data[0]
   end
 
   def to_html
-    @@compilers[filename.extname].call(self) rescue h($!)
+    (@@compilers.assoc(filename.extname) || @@compilers.last)[1].call(self) rescue h($!)
   end
 
   def self.find(pattern, options = {})
     pattern.gsub!(/[^\d\w_]/, '') # DON'T DELETE!!!
     pattern += '\.' if options[:complete_label]
-    limit = (options[:limit] || config[:limit] || 3).to_i
+    limit = options[:limit] || 10
     files = chdir(:entries) { Dir.glob("#{pattern}*") }.sort.reverse
-    (files[limit * options[:page].to_i, limit] || []).map {|e| Entry.new(e) }
+    (files[limit * (options[:page] || 0), limit] || []).map {|e| Entry.new(e) }
   end
 
-  @@compilers = Hash.new(lambda {|entry| entry.body })
-  def self.add_compiler(extname, &block)
-    @@compilers[extname] = block
+  @@compilers = [[nil, lambda {|entry| entry.body }]]
+  def self.add_compiler(extname = nil, &block)
+    @@compilers.unshift [extname, block]
   end
 end
 
---
+++ start.rb
@@ -0,0 +1,16 @@
+require 'rubygems'
+require 'sinatra'
+
+load './bilborc'
+setup_environment
+load_plugins
+
+get '/:date' do |date|
+  @entries = Entry.find(date)
+  haml :list
+end
+
+get '*' do
+  @entries = Entry.find('20')
+  haml :list
+end
