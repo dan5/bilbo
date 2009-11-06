4013bf446a11fb171293ce10d51941d9101b5e0a
dan5 <dan5.ya@gmail.com>
2009-11-06 13:56:05 +0900

    base_urlを書き直した

--- plugins/bilborss.rb
+++ plugins/bilborss.rb
@@ -20,54 +20,37 @@ class Entry
   end
 end
 
-def rss_uri_base
-  (
-    (config[:rss] || {})[:uri] ||
-    "http://#{ENV['HTTP_HOST']}#{ENV['REQUEST_URI']}"
-  ).sub(/[^\/]+$/, '')
-end
-
-def rss_uri
-  (config[:rss] || {})[:uri] || "#{rss_uri_base}rss"
-end
-
-def rss_filename
-  rss_uri.split('/').last
-end
-
-def base_url # from Hiki source
-  if !ENV['SCRIPT_NAME']
-    ''
-  elsif ENV['HTTPS'] && /off/i !~ ENV['HTTPS']
-    port = (ENV['SERVER_PORT'] == '443') ? '' : ':' + ENV['SERVER_PORT'].to_s
-    "https://#{ ENV['SERVER_NAME'] }#{ port }#{File::dirname(ENV['SCRIPT_NAME'])}/".sub(%r|/+$|, '/')
-  else
-    port = (ENV['SERVER_PORT'] == '80') ? '' : ':' + ENV['SERVER_PORT'].to_s
-    "http://#{ ENV['SERVER_NAME'] }#{ port }#{File::dirname(ENV['SCRIPT_NAME'])}/".sub(%r|/+$|, '/')
-  end
+# http://www.machu.jp/diary/20090818.html#p01
+def base_url(request)
+  default_port = (request.scheme == "http") ? 80 : 443
+  port = (request.port == default_port) ? "" : ":#{request.port.to_s}"
+  "#{request.scheme}://#{request.host}#{port}"
 end
 
 def generate_rss(entries, options = {})
+  base_path = request.script_name.sub(/\/?[^\/]*$/, '')
+  top = base_url(request) + base_path
   RSS::Maker.make("1.0") do |maker|
     entries.each do |entry|
       item = maker.items.new_item
-      item.link            = "#{rss_uri_base}permalink/#{entry.label}"
+      item.link            = "#{top}/permalink/#{entry.label}"
       item.description     = entry.rss_description
       item.title           = entry.rss_tile
       item.content_encoded = entry.rss_body
       item.date            = entry.time
     end
-    maker.channel.link  = options[:link] || rss_uri_base || base_url
+    maker.channel.link  = top
     maker.channel.about = options[:about] || "abaout"
     maker.channel.title = options[:title] || config[:title]
     maker.channel.description = options[:description] || config[:description] || 'Please set config[:description] in bilborc'
-  end.to_s
+  end.to_s + request.inspect
 end
 
 get '/rss' do
   generate_rss(Entry.find('20', :limit => 20))
 end
 
-add_plugin_hook(:header) {
-  %Q!<link rel="alternate" type="application/rss+xml" title="RSS" href="#{rss_filename}">!
+add_plugin_hook(:header) {|c|
+  base_path = c.request.script_name.sub(/\/?[^\/]*$/, '')
+  %Q!<link rel="alternate" type="application/rss+xml" title="RSS" href="#{base_path}/rss">!
 }
